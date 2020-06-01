5c95fcdaaa8155d4865eb416b84c8d58618ab2ef
Peter O <poccil14@gmail.com>
2020-06-01 07:33:22 -0400

    update Java version

--- src/main/java/com/upokecenter/cbor/CBOREInteger.java
+++ src/main/java/com/upokecenter/cbor/CBOREInteger.java
@@ -1,6 +1,6 @@
 package com.upokecenter.cbor;
 /*
-Written by Peter O.
+Written by Peter O. in 2014.
 Any copyright is dedicated to the Public Domain.
 http://creativecommons.org/publicdomain/zero/1.0/
 If you like this, you should donate to Peter O.
