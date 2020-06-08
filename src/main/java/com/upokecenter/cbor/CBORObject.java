5ff79b1e37288a9eb4a4f63bf3fba36d762e2da6
Peter O <poccil14@gmail.com>
2020-06-08 07:12:07 -0400

    Minor code edits

--- src/main/java/com/upokecenter/cbor/CBORObject.java
+++ src/main/java/com/upokecenter/cbor/CBORObject.java
@@ -1,6 +1,6 @@
 package com.upokecenter.cbor;
 /*
-Written by Peter O. in 2013-2018.
+Written by Peter O.
 Any copyright is dedicated to the Public Domain.
 http://creativecommons.org/publicdomain/zero/1.0/
 If you like this, you should donate to Peter O.
