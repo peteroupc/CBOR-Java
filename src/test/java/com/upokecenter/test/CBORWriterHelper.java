09bf77eb3566769e8931c76ae7b8e4367a57a819
Peter O <poccil14@gmail.com>
2020-12-04 18:47:09 -0500

    update Java version

---
+++ src/test/java/com/upokecenter/test/CBORWriterHelper.java
@@ -0,0 +1,67 @@
+package com.upokecenter.test;
+
+import java.io.*;
+import com.upokecenter.cbor.*;
+
+  /**
+   * Helper class to write CBOR objects using a "fluent" interface. Inspired by
+   *  suggestions by GitHub user "sbernard31".
+   */
+  public final class CBORWriterHelper {
+    private OutputStream outputStream;
+
+    public CBORWriterHelper(OutputStream outputStream) {
+      this.outputStream = outputStream;
+    }
+
+    public CBORWriterHelper WriteStartArray(int size) throws java.io.IOException {
+      CBORObject.WriteValue(this.outputStream, 4, size);
+      return this;
+    }
+
+    public CBORWriterHelper WriteStartMap(int size) throws java.io.IOException {
+      CBORObject.WriteValue(this.outputStream, 5, size);
+      return this;
+    }
+
+    public CBORWriterHelper WriteStartArray(long size) throws java.io.IOException {
+      CBORObject.WriteValue(this.outputStream, 4, size);
+      return this;
+    }
+
+    public CBORWriterHelper WriteStartMap(long size) throws java.io.IOException {
+      CBORObject.WriteValue(this.outputStream, 5, size);
+      return this;
+    }
+
+    public CBORWriterHelper WriteStartArray() throws java.io.IOException {
+      this.outputStream.write((int)0x9f);
+      return this;
+    }
+
+    public CBORWriterHelper WriteStartMap() throws java.io.IOException {
+      this.outputStream.write((int)0xbf);
+      return this;
+    }
+
+    public CBORWriterHelper WriteEndArray() throws java.io.IOException {
+      this.outputStream.write((int)0xff);
+      return this;
+    }
+
+    public CBORWriterHelper WriteEndMap() throws java.io.IOException {
+      this.outputStream.write((int)0xff);
+      return this;
+    }
+
+    public CBORWriterHelper Write(Object key, Object value) throws java.io.IOException {
+      CBORObject.Write(key, this.outputStream);
+      CBORObject.Write(value, this.outputStream);
+      return this;
+    }
+
+    public CBORWriterHelper Write(Object value) throws java.io.IOException {
+      CBORObject.Write(value, this.outputStream);
+      return this;
+    }
+  }
