43feb78c37b68cd8ac9d992183dbb22a991c5a64
Peter O <poccil14@gmail.com>
2021-04-07 08:14:17 -0400

    add missing source file

---
+++ src/test/java/com/upokecenter/test/CPOD3.java
@@ -0,0 +1,13 @@
+package com.upokecenter.test;
+
+    public class CPOD3 {
+       public final String getAa() { return propVaraa; }
+public final void setAa(String value) { propVaraa = value; }
+private String propVaraa;
+       public final String getBb() { return propVarbb; }
+public final void setBb(String value) { propVarbb = value; }
+private String propVarbb;
+       public final String getCc() { return propVarcc; }
+public final void setCc(String value) { propVarcc = value; }
+private String propVarcc;
+    }
