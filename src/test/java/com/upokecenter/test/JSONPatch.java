4953eb8294e8a6e499ac4ac66586d4d444e13140
Peter O <poccil14@gmail.com>
2021-05-28 07:25:05 -0400

    edit .gitignore; etc.

--- src/test/java/com/upokecenter/test/JSONPatch.java
+++ src/test/java/com/upokecenter/test/JSONPatch.java
@@ -6,8 +6,6 @@ In case this is not possible, this work is also
 licensed under Creative Commons Zero (CC0):
 http://creativecommons.org/publicdomain/zero/1.0/
 
-If you like this, you should donate to Peter O.
-at: http://peteroupc.github.io/
 */
 
 import java.util.*;
