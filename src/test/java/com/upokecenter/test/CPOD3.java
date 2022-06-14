b2e38782237c912130091b769c729c9d4cef5150
Peter Occil <poccil14@gmail.com>
2022-06-14 06:21:49 -0400

    update Java version

--- src/test/java/com/upokecenter/test/CPOD3.java
+++ src/test/java/com/upokecenter/test/CPOD3.java
@@ -1,13 +1,11 @@
-package com.upokecenter.test;
+package com.upokecenter.test; import com.upokecenter.util.*;
+from fractions import Fraction
+import math
 
-    public class CPOD3 {
-       public final String getAa() { return propVaraa; }
-public final void setAa(String value) { propVaraa = value; }
-private String propVaraa;
-       public final String getBb() { return propVarbb; }
-public final void setBb(String value) { propVarbb = value; }
-private String propVarbb;
-       public final String getCc() { return propVarcc; }
-public final void setCc(String value) { propVarcc = value; }
-private String propVarcc;
-    }
+def chebtobern(n):
+   mat=[[0 for i in range(n + 1)] for j in range(n + 1)]
+   for j in range(n + 1): den = Fraction(1, math.comb(n, j))
+      for k in range(n + 1): mn = max(0, j + k-n)
+         mx = min(j, k) s = sum((-1)**(k-i)*math.comb(2*k, 2*i)*math.comb(n-k, j-i) \
+            for i in range(mn, mx + 1)) mat.get(j)[k]=s/den
+   return Matrix(mat)
